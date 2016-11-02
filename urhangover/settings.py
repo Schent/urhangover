6891c9ebdb2c842bb23d526bb236c11798bd1651
RenniW <krikchai.p@gmail.com>
2016-11-02 23:32:02 +0700

    แก้ indent

--- urhangover/settings.py
+++ urhangover/settings.py
@@ -77,8 +77,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'urhangover',
-	'USER': 'root',
-	'HOST': 'localhost',
+        'USER': 'root',
+        'HOST': 'localhost',
     }
 }
 
